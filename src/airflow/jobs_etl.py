3c13614b5b770faa98ddd136a7610c3c35f1c859
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-08-14 10:10:53 +0300

    chore: rearrange files

---
+++ src/airflow/jobs_etl.py
@@ -0,0 +1,47 @@
+from datetime import timedelta,datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from data.S3Pusher import uploadToS3
+from Extract import extract_rates
+from Extract import extract_datas
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date':'airflow.utils.dates.days_ago(2)',
+    'depends_on_past': False,
+    'email':['hei.nykoloina.2@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+jobs_etl_dags = DAG(
+    'jobs_etl_dags',
+    default_args=default_args,
+    description='ETL jobs for Data',
+    schedule_interval ='@daily',
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=60),
+    tags=['example,jobsetl']
+)
+
+
+def main():
+    apps = extract_datas('googleplaystore.csv')
+    reviews = extract_datas('googleplaystore_user_reviews.csv')
+    rates = extract_rates(
+        'https://v6.exchangerate-api.com/v6/85a67b762af150352ffb7e6a/latest/USD')
+
+# name as 'data.csv' which is the name of this file in S3
+# uploadToS3(name,'cleared_data.csv')
+
+def print_hello():
+    return 'Hello world!'
+
+start_task = DummyOperator(task_id='start_task',dag=hello_world_dag)
+hello_world_task = PythonOperator(task_id='hello_world_task',python_callable=print_hello,dag=hello_world_dag)
+end_task = DummyOperator(task_id='end_task', dag=hello_world_dag)
+
+start_task >> hello_world_task >> end_task
