9bd780c96112c9cf3f2ef5d6e5f93a154593e21b
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-08-14 11:19:48 +0300

    chore: rates url

--- src/data/S3Pusher.py
+++ src/data/S3Pusher.py
@@ -12,8 +12,6 @@ def uploadToS3(data,csvFilePath):
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
         aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
     )
-
-    
     # Convert instant now to string
     current_datetime_string = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
