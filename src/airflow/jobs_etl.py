2ca36622c5e3712bae470aef4eb2edd59dd1c308
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-08-14 10:23:26 +0300

    chore rearrange file v2

--- src/airflow/jobs_etl.py
+++ src/airflow/jobs_etl.py
@@ -27,16 +27,6 @@ jobs_etl_dags = DAG(
     tags=['example,jobsetl']
 )
 
-
-def main():
-    apps = extract_datas('googleplaystore.csv')
-    reviews = extract_datas('googleplaystore_user_reviews.csv')
-    rates = extract_rates(
-        'https://v6.exchangerate-api.com/v6/85a67b762af150352ffb7e6a/latest/USD')
-
-# name as 'data.csv' which is the name of this file in S3
-# uploadToS3(name,'cleared_data.csv')
-
 def print_hello():
     return 'Hello world!'
 
